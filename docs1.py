f97631c9973d42e20a04fa67376164321d1aa8e7
Shakeeb Shaikh <eren.85yeager@gmail.com>
2026-01-01 19:02:49 +0530

    Create initial documentation for programming language
    Added initial documentation for a programming language project.

---
+++ docs1.py
@@ -0,0 +1,14 @@
+r"""
+S# : Started to making my own programming language.
+
+#Problem Statement:well i was thinking how did some one made the programming languages just for understanding how actually did they programmed a programm to program a program and just this curiosity sparked me to make one using python...
+
+
+
+
+#Goal:Just for funnn...
+
+S# : Fun fact is i am writing an interprated language  in an interprated language🤣🤣🤣 just imagine the time it will take to complie and interpret any thing TC will be 📈📈📈...
+
+
+"""
